a1c0b88932d570f1f213d0c8baf4474d5ffe7455
Paulo <124407850+PauloSantos2002@users.noreply.github.com>
2025-09-07 23:34:35 -0300

    media query finalização JavaScript

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ function calcular() {
 
     let dataOutput = new Date()
     let dataInput = new Date(inputYear, inputMonth - 1, inputDay)
-    let difTempo = dataOutput.getTime() - dataInput.getTime()
-    let outputDay = Math.ceil(difTempo / (24*60*60*1000))
-    let outputMounth = Math.ceil(outputDay / 30)
-    let outputYers = Math.ceil(outputMounth / 12)
+    
+    dataOutput.setDate(dataOutput.getDate() - dataInput.getDate())
+    dataOutput.setMonth(dataOutput.getMonth() - dataInput.getMonth())
+    dataOutput.setFullYear(dataOutput.getFullYear() - dataInput.getFullYear())
+
 
 
     if (inputDay > 31 || inputMonth > 12) {
         console.log('Data inválida')
         stop
     }else{
-        document.getElementById('day_output').innerHTML =   outputDay
-        document.getElementById('month_output').innerHTML =   outputMounth
-        document.getElementById('year_output').innerHTML =   outputYers
-        console.log(outputDay )
+        document.getElementById('day_output').innerHTML =  dataOutput.getDate()
+        document.getElementById('month_output').innerHTML =   dataOutput.getMonth()
+        document.getElementById('year_output').innerHTML =   dataOutput.getFullYear()
     }
 
     
